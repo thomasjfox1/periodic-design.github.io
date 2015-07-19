6860daedb2b5e6d877cf5af4aae31a78d48ddb95
Thomas J. Fox <thomasjfox1@gmail.com>
2015-07-19 16:51:54 -0500

    added center logo and some scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,29 @@
 // javascript wizardry
+
+$(function(){
+  $('.center-logo').css('position','absolute');
+  $('.center-logo').css('top', Math.max(0, (($(window).height() - $('.center-logo').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+  $('.center-logo').css('left', Math.max(0, (($(window).width() - $('.center-logo').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+
+  $(window).resize(function() {
+    $('.center-logo').css('position','absolute');
+    $('.center-logo').css('top', Math.max(0, (($(window).height() - $('.center-logo').outerHeight()) / 2) + $(window).scrollTop()) + "px");
+    $('.center-logo').css('left', Math.max(0, (($(window).width() - $('.center-logo').outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+  });
+});
+
+$(function(){
+  var viewportWidth = $(window).width();
+  var viewportHeight = $(window).height();
+
+  $('body').css('height', viewportHeight + 'px');
+  $('body').css('width', viewportWidth + 'px');
+
+  $(window).resize(function() {
+      var viewportWidth = $(window).width();
+      var viewportHeight = $(window).height();
+
+      $('body').css('height', viewportHeight + 'px');
+      $('body').css('width', viewportWidth + 'px');
+  });
+});
